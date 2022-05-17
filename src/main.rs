9349943252aa4f521386634c38c1f93b2c866b8a
samz <samjzhu@gmail.com>
2022-05-17 14:09:25 -0400

    Refactoring: reduced dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,34 @@
 use clap::{arg, Command};
-use tokio::time::{sleep, Duration};
-use rand;
-use rand::Rng;
-use mouse_rs::Mouse;
-use rdev::{simulate, EventType, SimulateError, display_size};
+use std::time::Duration;
+use enigo::*;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let matches = Command::new("awake")
         .arg(
-            arg!([INTERVAL] ).default_value("30")
+            arg!([INTERVAL] ).default_value("15")
                 .help("Interval time to check in seconds"),
         )
         .get_matches();
     let interval :u64 = matches.value_of_t("INTERVAL").unwrap();
     println!("interval:{}", interval);
     let period = Duration::from_secs(interval);
-    let mut rng = rand::thread_rng();
-    let (w, h) = display_size().unwrap();
-    let x = w as i32;
-    let y = h as i32;
-    println!("Screen size:{} x {}", x, y);
-    let mouse = Mouse::new();
+    let mut enigo = Enigo::new();
     loop {
-        let pos = mouse.get_position().unwrap();
-        sleep(period).await;
-        let pos1 = mouse.get_position().unwrap();
-        if pos.x == pos1.x && pos.y == pos1.y {
-            let pos_x:i32 = rng.gen_range(0..x);
-            let pos_y:i32 = rng.gen_range(0..y);
-            match simulate(&EventType::MouseMove {x: f64::from(pos_x), y: f64::from(pos_y) }){
-                Ok(()) => (),
-                Err(SimulateError) => {
-                    println!("We could not send MouseMove");
-                }
-            }
-            let pos1 = mouse.get_position().unwrap();
-            println!("X: {}, Y: {}", pos1.x, pos1.y);
+        let (pos_x, pos_y) = Enigo::mouse_location();
+        println!("X: {}, Y: {}", pos_x, pos_y);
+        std::thread::sleep(period);
+        let (pos1_x, pos1_y) = Enigo::mouse_location();
+        if pos_x == pos1_x && pos_y == pos1_y {
+            let pos_dx = match pos_x {
+                dx if dx < 1000 => 10,
+                _ => -16
+            };
+            let pos_dy = match pos_y {
+                dy if dy < 600 => 4,
+                _ => -16
+            };
+            println!("dx: {}, dy: {}", pos_dx, pos_dy);
+            enigo.mouse_move_relative(pos_dx, pos_dy);
         }
     }
 }
